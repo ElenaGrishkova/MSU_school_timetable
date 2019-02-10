ae18c9df2ca2bf8d160531602e1bdce103a06a23
ЛеГо <->
2019-02-10 15:34:43 +0300

    for jar

--- src/main/java/shedule/Main.java
+++ src/main/java/shedule/Main.java
@@ -6,6 +6,7 @@ import excel.TimetableExcelHelper;
 import org.apache.poi.ss.usermodel.*;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStreamReader;
 import java.util.Map;
@@ -13,10 +14,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        InputStreamReader inputFile = null;
+        String inputFileName = null;
+        if (0 < args.length) {
+            inputFileName = args[0];
+        } else {
+            System.err.println("Invalid arguments count:" + args.length);
+            System.exit(1);
+        }
 //        InputStreamReader inputFile = new InputStreamReader(Main.class.getClassLoader()
 //                .getResourceAsStream("123.xml"), "windows-1251");
-        InputStreamReader inputFile = new InputStreamReader(Main.class.getClassLoader()
-                .getResourceAsStream("18week.xml"), "windows-1251");
+//        InputStreamReader inputFile = new InputStreamReader(Main.class.getClassLoader()
+//                .getResourceAsStream("18week.xml"), "windows-1251");
+        inputFile = new InputStreamReader(new FileInputStream(inputFileName), "windows-1251");
 
         Scanner scan = new Scanner(inputFile);
         StringBuilder xml = new StringBuilder();
@@ -34,7 +44,8 @@ public class Main {
         TimetableData timetableData = new TimetableData(timetable);
         timetableData.process();
 
-        Workbook wb = WorkbookFactory.create(new File("template.xlsx"));
+        Workbook wb = WorkbookFactory.create(Main.class.getClassLoader()
+               .getResourceAsStream("template.xlsx"));
         TimetableExcelHelper helper = new TimetableExcelHelper();
         Map<String, Cell> cellIndex_8 = helper.createCellIndex(wb, 0);
         Map<String, Cell> cellIndex_10_11 = helper.createCellIndex(wb, 1);
